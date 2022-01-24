89f417901d1463e8841e007f4749024438cd7e3a
Ji-eun <tori@ryanlab.kr>
2022-01-24 18:29:12 +0900

    코로나 백신접종현황 api 생성 중

--- src/Molecules/BarLineChart.js
+++ src/Molecules/BarLineChart.js
@@ -1,8 +1,24 @@
-import React from 'react'
 import { Bar, Chart } from 'react-chartjs-2'
 import 'chart.js/auto'; 
+import { useState, useEffect } from 'react'
 
 const BarLineChart = () => {
+
+    const getData = async() => {
+        const response = await fetch(
+            
+            'https://api.odcloud.kr/api/15077756/v1/vaccine-stat?page=1&perPage=10&returnType=JSON&serviceKey=ncMhlOzEE8Q%2FpSEqd1XItWoN%2FBsvypkNC1vzjYNRRGcABDkRcAXcQd9wOxSfX2G6%2BPULf5YtcyON%2FFzAZrfdpg%3D%3D'
+            /*
+           'https://api.odcloud.kr/api/15077756/v1/vaccine-stat?page=1&perPage=10&cond%5BbaseDate%3A%3AEQ%5D=2022-01-24&cond%5BbaseDate%3A%3ALT%5D=2022-01-24&cond%5BbaseDate%3A%3ALTE%5D=2022-01-24&cond%5BbaseDate%3A%3AGT%5D=2022-01-24&cond%5BbaseDate%3A%3AGTE%5D=2022-01-24&serviceKey=%09%20ncMhlOzEE8Q%2FpSEqd1XItWoN%2FBsvypkNC1vzjYNRRGcABDkRcAXcQd9wOxSfX2G6%2BPULf5YtcyON%2FFzAZrfdpg%3D%3D'
+            */
+        );
+        const json = await response.json();
+        console.log(json);
+    }
+
+    useEffect(() => {
+        getData();
+    }, [])
 
     const data = {
         labels: [
