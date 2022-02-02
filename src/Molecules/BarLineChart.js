4489e62f296612f330e64ce616fca237f4dcf28b
Ji-eun <tori@ryanlab.kr>
2022-02-02 15:37:03 +0900

    scatter chart 완성

--- src/Molecules/BarLineChart.js
+++ src/Molecules/BarLineChart.js
@@ -1,13 +1,19 @@
 import { Bar, Chart } from 'react-chartjs-2'
 import 'chart.js/auto'; 
 import { useState, useEffect } from 'react'
+import axios from 'axios';
+import XMLParser from 'react-xml-parser';
 
 const BarLineChart = () => {
+
+    const [newDecideCntArr, setNewDecideCntArr] = useState([]);
+    const [newDeathCntArr, setNewDeathCntArr] = useState([]);
 
     const moment = require('moment');
     const today = moment();
 
-    const format = today.format('YYYY-MM-DD');
+    const format = today.format('YYYYMMDD');
+    const sevenDaysAgoFormat = moment().subtract(7, 'days').format('YYYYMMDD');
 
     const todayFormat = today.format('MM.DD');
     const oneDayAgo = moment().subtract(1, 'days').format('MM.DD');
@@ -17,30 +23,40 @@ const BarLineChart = () => {
     const fiveDaysAgo = moment().subtract(5, 'days').format('MM.DD');
     const sixDaysAgo = moment().subtract(6, 'days').format('MM.DD');
 
-    {/* 백신 확진자 현황 api - SERVICE KEY IS NOT REGISTERED ERROR ( 시간 지나야 사용할 수 있는지 확인이 필요함. )*/}
+    {/* 백신 확진자 현황 api - api호출을 여러번 할 수 없나?? */}
     const getData = () => {
-      var xhr = new XMLHttpRequest();
-      var url = 'http://openapi.data.go.kr/openapi/service/rest/Covid19/getCovid19InfStateJson'; /*URL*/
-      // 일반 인증키(Encoding) ncMhlOzEE8Q%2FpSEqd1XItWoN%2FBsvypkNC1vzjYNRRGcABDkRcAXcQd9wOxSfX2G6%2BPULf5YtcyON%2FFzAZrfdpg%3D%3D
-      // 일반 인증키(Decoding) ncMhlOzEE8Q/pSEqd1XItWoN/BsvypkNC1vzjYNRRGcABDkRcAXcQd9wOxSfX2G6+PULf5YtcyON/FzAZrfdpg==
-      var queryParams = '?' + encodeURIComponent('serviceKey') + '='+'ncMhlOzEE8Q/pSEqd1XItWoN/BsvypkNC1vzjYNRRGcABDkRcAXcQd9wOxSfX2G6+PULf5YtcyON/FzAZrfdpg=='; /*Service Key*/
-      queryParams += '&' + encodeURIComponent('pageNo') + '=' + encodeURIComponent('1'); /**/
-      queryParams += '&' + encodeURIComponent('numOfRows') + '=' + encodeURIComponent('10'); /**/
-      queryParams += '&' + encodeURIComponent('startCreateDt') + '=' + encodeURIComponent('20220110'); /**/
-      queryParams += '&' + encodeURIComponent('endCreateDt') + '=' + encodeURIComponent('20220110'); /**/
-      xhr.open('GET', url + queryParams);
-      xhr.onreadystatechange = function () {
-          if (this.readyState == 4) {
-              console.log(xhr.response);
-              // alert('Status: '+this.status+'nHeaders: '+JSON.stringify(this.getAllResponseHeaders())+'nBody: '+this.responseText);
-          }
-      };
-      
-      xhr.send('');
+        var xhr = new XMLHttpRequest();
+        var url = 'http://openapi.data.go.kr/openapi/service/rest/Covid19/getCovid19InfStateJson'; /*URL*/
+        var queryParams = '?' + encodeURIComponent('serviceKey') + '='+'ncMhlOzEE8Q%2FpSEqd1XItWoN%2FBsvypkNC1vzjYNRRGcABDkRcAXcQd9wOxSfX2G6%2BPULf5YtcyON%2FFzAZrfdpg%3D%3D'; /*Service Key*/
+        queryParams += '&' + encodeURIComponent('pageNo') + '=' + encodeURIComponent('1'); /**/
+        queryParams += '&' + encodeURIComponent('numOfRows') + '=' + encodeURIComponent('10'); /**/
+        queryParams += '&' + encodeURIComponent('startCreateDt') + '=' + encodeURIComponent(sevenDaysAgoFormat); /**/
+        queryParams += '&' + encodeURIComponent('endCreateDt') + '=' + encodeURIComponent(format); /**/
+        xhr.open('GET', url + queryParams);
+        xhr.onreadystatechange = function () {
+            if (this.readyState == 4) {
+                var XMLParser = require('react-xml-parser')
+                var xml = new XMLParser().parseFromString(xhr.response); 
+
+                let decideCntArr = [];
+                let deathCntArr = [];
+                let newDecideCntArr = [];
+                for(let i=0; i<8; i++){
+                  decideCntArr.unshift(xml.children[1].children[0].children[i].children[3].value);
+                  deathCntArr.unshift(xml.children[1].children[0].children[i].children[2].value);
+                }
+                for(let i=0; i<decideCntArr.length-1; i++){
+                  newDecideCntArr.push(decideCntArr[i+1] - decideCntArr[i])
+                }
+                setNewDecideCntArr(newDecideCntArr);
+                setNewDeathCntArr(deathCntArr);
+            }
+        };
+        xhr.send('');
     }
 
     useEffect(() => {
-      getData();
+      getData();   
     }, [])
 
     const data = {
@@ -56,13 +72,13 @@ const BarLineChart = () => {
         datasets: [{
           type: 'bar',
           label: '신규 확진자수',
-          data: [10, 20, 30, 40, 50, 60, 70],
+          data: newDecideCntArr,
           borderColor: 'rgb(255, 99, 132)',
           backgroundColor: 'rgba(255, 99, 132, 0.2)'
         }, {
           type: 'line',
-          label: '7일 평균',
-          data: [10, 20, 30, 40, 60, 70, 100],
+          label: '누적 사망자',
+          data: newDeathCntArr,
           fill: false,
           borderColor: 'rgb(54, 162, 235)'
         }]
