62aeeaed260bd67a7bcd100d657ba16b5b88adeb
Ji-eun <tori@ryanlab.kr>
2022-02-02 23:29:15 +0900

    news api 수정

--- src/pages/News/News.js
+++ src/pages/News/News.js
@@ -5,44 +5,23 @@ import axios from 'axios';
 export default function News() {
 
     const [news, setNews] = useState([]);
-
+    
+    // 이게 더 맞는 문서 같음 --> https://developers.naver.com/docs/search/news/  ,  https://luckygg.tistory.com/307
     // https://developers.naver.com/docs/serviceapi/datalab/search/search.md#%ED%86%B5%ED%95%A9-%EA%B2%80%EC%83%89%EC%96%B4-%ED%8A%B8%EB%A0%8C%EB%93%9C
-    //  Documents 진짜 모르겠다~~
     const getNews = async () => {
 
         var client_id = 'hWTkdXKVzGYiHCarA5br';
         var client_secret = 'hojxkLdOG0';
         var request_body = {
-            "startDate": "2017-01-01",
-            "endDate": "2017-04-30",
-            "timeUnit": "month",
-            "keywordGroups": [
-                {
-                    "groupName": "한글",
-                    "keywords": [
-                        "한글",
-                        "korean"
-                    ]
-                },
-                {
-                    "groupName": "영어",
-                    "keywords": [
-                        "영어",
-                        "english"
-                    ]
-                }
-            ],
-            "device": "pc",
-            "ages": [
-                "1",
-                "2"
-            ],
-            "gender": "f"
+            "query": "vaccine",
+            "display": "10",
+            "start": "2",
+            "sort": "date"
         };
         
 
         try{
-            const response = await axios.post('https://openapi.naver.com/v1/datalab/search', {
+            const response = await axios.get('https://openapi.naver.com/v1/search/news.json', {
                 params: JSON.stringify(request_body),
                 headers:{
                     'X-Naver-Client-Id': client_id,
