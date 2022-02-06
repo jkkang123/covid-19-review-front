c12a3013695b7beaea639a1cc96d2cebf88655d9
Ji-eun <tori@ryanlab.kr>
2022-02-06 22:59:52 +0900

    news.js pagination 추가

--- src/pages/News/News.js
+++ src/pages/News/News.js
@@ -1,14 +1,21 @@
 import { useState, useEffect } from 'react';
 import React from 'react'
 import axios from 'axios';
+import Pagination from '@mui/material/Pagination';
+import Stack from '@mui/material/Stack';
 
-// replace --> title & description 모두 이상한 문자들이 ( <b>, </b>, &quot; ) 떠오르는데, 직접 replace를 넣으면 작동하지만 함수로 빼려고 하니 작동이 안됨!
-
+// pagination의 숫자를 클릭하면 자동으로 색상이 들어옴, 그런데 selectBox를 Change했을 때 pagination만 변하지 않는다. 연동하는 방법은 ?
+// <Pagination page={ page } /> --> 실패. 왜??
 export default function News() {
 
     const [news, setNews] = useState([]);
     const [query, setQuery] = useState('화이자');
+    const [page, setPage] = useState(1);
+    const [total, setTotal] = useState();
+    const [pageCounts, setPageCounts] = useState()
     const moment = require('moment');
+    const display = 10;
+    const initial = 1;
 
     // 선택한 option에 따라 query를 바꾸는 함수 querySelector
     const querySelector = (e) => {
@@ -21,14 +28,19 @@ export default function News() {
         }
     }
 
+    // 첫페이지로 이동하는 함수 jumpFirst
+    const jumpFirst = () => {
+        setPage(1);
+    }
+
     // 백신 뉴스 api
     const getNews = async () => {
         var client_id = 'hWTkdXKVzGYiHCarA5br';
         var client_secret = 'hojxkLdOG0';
         var request_body = {
             query: query,
-            display: 10,
-            start: 1,
+            display: display,
+            start: page,
             sort: 'date'
         };
         
@@ -42,18 +54,28 @@ export default function News() {
                 }
             });
             setNews(response.data.items);
+            setTotal(response.data.total)
         } catch (e) {
             console.log(e);
         }
     }
 
+    // <b>, &quot;, </b> 지우는 함수 cleanString
+    const cleanString = (value) => {
+        return value
+        .replace(/<b>/g, "")
+        .replace(/&quot;/g, "")
+        .replace(/<\/b>/gi, "")
+    }
+   
     useEffect(() => {
         getNews();
-    }, [query])
+        setPageCounts(Math.ceil(total / display));
+    }, [query,page,total])
 
     return (
         <div>
-            <select onChange = { querySelector }>
+            <select onChange = { e => { querySelector(e); jumpFirst(); } }>
                 <option value="화이자">화이자</option>
                 <option value="모더나">모더나</option>
                 <option value="부스터샷">부스터샷</option>
@@ -63,22 +85,8 @@ export default function News() {
                 news.map((item, i) => {
                     return(
                         <div key={i}>
-                            <h1>
-                                { 
-                                    item.title 
-                                    .replace(/<b>/g, "")
-                                    .replace(/&quot;/g, "")
-                                    .replace(/<\/b>/gi, "")
-                                }
-                            </h1>
-                            <p>
-                                { 
-                                    item.description 
-                                    .replace(/<b>/g, "")
-                                    .replace(/&quot;/g, "")
-                                    .replace(/<\/b>/gi, "")
-                                }
-                            </p>
+                            <h1>{ cleanString(item.title) }</h1>
+                            <p>{ cleanString(item.description) }</p>
                             <span>{ moment(item.pubDate).format('YYYY. MM. DD.') }</span>
                             
                             <hr/>
@@ -86,6 +94,15 @@ export default function News() {
                     )
                 })
             }
+
+            <Stack spacing={2}>
+                <Pagination 
+                    onChange={ (e,value) => {setPage(value)} } 
+                    count={ pageCounts } 
+                    color="primary" 
+                    page={ page }
+                />
+            </Stack>
         </div>
     )
 }
